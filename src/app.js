1b39fe5501ba90086a16b493ad41c9da911635ee
Jaime Palacio <jaime_lorenzo_palacio@dlsl.edu.ph>
2026-07-22 21:28:21 +0800

    GT4: Add async functions to app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import { formatDate, validateTask, mergeTaskUpdate} from './utils.js';
-
+import { fetchSampleUsers } from './api.js';
+import { formatDate, validateTask, mergeTaskUpdate, createTask, TaskValidationError } from './utils.js';
 console.log('Server starting...');
 
 const formattedDate = formatDate(new Date("2026-07-22"));
@@ -13,3 +13,29 @@ console.log(origTask)
 const updatedTask = { title: "Task 1 (Updated)" };
 const uptResult = mergeTaskUpdate(origTask, updatedTask);
 console.log(uptResult);
+
+async function runThis() {
+  try {
+    console.log("Fetching users:");
+    const users = await fetchSampleUsers();
+    console.log("Users Fetched:", users);
+
+    console.log("Creating task:");
+    const sampleTask = {
+      title: "Inspect LGU Asset Inventory",
+      dueDate: new Date("2026-08-01"),
+    };
+
+    const createdTask = createTask(sampleTask);
+    console.log("Task has been successfully created:", createdTask, sampleTask);
+
+  } catch (error) {
+    if (error instanceof TaskValidationError) {
+      console.error("Task Validation Error:", error.message);
+    } else {
+      console.error("An unexpected error occurred:", error);
+    }
+  }
+}
+
+runThis();
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,25 @@
 export const formatDate = (date) => `Due: ${date.toLocaleDateString()}`;
-export const validateTask = ({ title, dueDate } = {}) => Boolean(title && dueDate);export const mergeTaskUpdate = (original, ...updates) => ({
+export const validateTask = ({ title, dueDate } = {}) => Boolean(title && dueDate);
+export const mergeTaskUpdate = (original, ...updates) => ({
   ...original,
   ...updates.reduce((old, curr) => ({ ...old, ...curr }), {})
 });
+
+export class TaskValidationError extends Error{
+  constructor(message){
+    super(message);
+    this.name = "TaskValidationError";
+  }
+}
+export function createTask(taskData) { 
+  if (!validateTask(taskData)) { 
+    throw new TaskValidationError("Invalid task data"); 
+
+  return {
+    id: Date.now(),
+    completed: false,
+    ...taskData,
+    };
+  }
+}
 
